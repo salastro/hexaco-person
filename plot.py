2e228fdee4bacaad83093c7f192caf116153d1a6
salastro <salah2112004@gmail.com>
2023-01-17 13:06:19 +0200

    Use variables from model.py

--- plot.py
+++ plot.py
@@ -2,52 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+from model import stats
 
 filename = input("CSV file: ")
 data = pd.read_csv(
     rf'{filename}.csv',
     index_col=0,
 ).squeeze("rows").to_dict()
-
-""" Data from https://hexaco.org/hexaco-inventory. """
-stats = {
-    'h': {
-        'domain': 'Honesty',
-        'mean': 3.19,
-        'stdev': 0.62,
-        'color': 'orange',
-    },
-    'e': {
-        'domain': 'Emotionality',
-        'mean': 3.43,
-        'stdev': 0.62,
-        'color': 'green',
-    },
-    'x': {
-        'domain': 'Extraversion',
-        'mean': 3.50,
-        'stdev': 0.57,
-        'color': 'blue',
-    },
-    'a': {
-        'domain': 'Agreeableness',
-        'mean': 2.94,
-        'stdev': 0.58,
-        'color': 'red',
-    },
-    'c': {
-        'domain': 'Conscientiousness',
-        'mean': 3.44,
-        'stdev': 0.56,
-        'color': 'grey',
-    },
-    'o': {
-        'domain': 'Openness',
-        'mean': 3.41,
-        'stdev': 0.60,
-        'color': 'violet',
-    },
-}
 
 fig, ax = plt.subplots(2, 3)
 score_range = np.arange(1, 5, 0.01)
